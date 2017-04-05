9ad9fe4544d4b2c52faef718b0c9b1756f6c75f9
Dawit Abraham <davescofab@gmail.com>
2017-04-05 19:56:33 +0300

    Add the PhotoHolder and PhotoAdapter

--- app/src/main/java/com/qenetech/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/qenetech/photogallery/PhotoGalleryFragment.java
@@ -10,10 +10,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.qenetech.photogallery.backend.FlickrFetchr;
+import com.qenetech.photogallery.model.GalleryItem;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by davescof on 4/4/17.
@@ -45,6 +48,51 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private void setAdapter (){
+        if (isAdded())
+        {
+            
+        }
+    }
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView;
+        }
+
+        public void bindPhoto (GalleryItem item){
+            mTextView.setText(item.getCaption());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder>{
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter (List<GalleryItem> galleryItems){
+            mGalleryItems = galleryItems;
+        }
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new PhotoHolder(new TextView(getActivity()));
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            GalleryItem item = mGalleryItems.get(position);
+            holder.bindPhoto(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
     }
 
     private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
