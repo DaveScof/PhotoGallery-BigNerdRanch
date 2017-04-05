ec414254b37a6ecfe6ebd62f121ccd633f6a237d
Dawit Abraham <davescofab@gmail.com>
2017-04-05 19:27:08 +0300

    Add GalleryItem model

---
+++ app/src/main/java/com/qenetech/photogallery/model/GalleryItem.java
@@ -0,0 +1,40 @@
+package com.qenetech.photogallery.model;
+
+/**
+ * Created by davescof on 4/5/17.
+ */
+
+public class GalleryItem {
+    private String mId;
+    private String mCaption;
+    private String mUrl;
+
+    public GalleryItem (String caption)
+    {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}
