d0b74ff3d4a13dbdd266970154e18bb14d9cc433
Dawit Abraham <davescofab@gmail.com>
2017-04-05 19:56:14 +0300

    Parse the photo Json Objects and add them to a GalleryItem list

--- app/src/main/java/com/qenetech/photogallery/backend/FlickrFetchr.java
+++ app/src/main/java/com/qenetech/photogallery/backend/FlickrFetchr.java
@@ -3,6 +3,9 @@ package com.qenetech.photogallery.backend;
 import android.net.Uri;
 import android.util.Log;
 
+import com.qenetech.photogallery.model.GalleryItem;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,6 +16,8 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by davescof on 4/4/17.
@@ -22,6 +27,8 @@ public class FlickrFetchr {
 
     public static final String TAG = "FlickrFetchr";
     public static final String API_KEY = "9064993332fbc73e2a7588f7d74746fd";
+
+    List<GalleryItem> mGalleryItems = new ArrayList<>();
 
 
     public byte[] getUrlBytes (String urlSpec) throws IOException {
@@ -66,6 +73,8 @@ public class FlickrFetchr {
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received JSON: " + jsonString);
             JSONObject jsonObject = new JSONObject(jsonString);
+
+            parseGalleryItems(mGalleryItems, jsonObject);
         } catch (IOException e) {
             Log.e(TAG, "Failed to fetch items", e);
         } catch (JSONException e) {
@@ -73,6 +82,23 @@ public class FlickrFetchr {
         }
     }
 
+    private void parseGalleryItems (List<GalleryItem> items, JSONObject jsonObject) throws JSONException {
+        JSONObject photosJsonObject = jsonObject.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s"))
+                continue;
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
+        }
+    }
 
 
     public String getUrlString (String urlSpec) throws IOException{
