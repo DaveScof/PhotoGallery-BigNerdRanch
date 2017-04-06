754e4b428ca91ca26f301f4732476f0f5cf61a0c
Dawit Abraham <davescofab@gmail.com>
2017-04-06 23:12:47 +0300

    Add requestHandler and ConcurrentMap to ThumbnailDownloader

--- app/src/main/java/com/qenetech/photogallery/backend/ThumbnailDownloader.java
+++ app/src/main/java/com/qenetech/photogallery/backend/ThumbnailDownloader.java
@@ -1,7 +1,11 @@
 package com.qenetech.photogallery.backend;
 
+import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by davescof on 4/6/17.
@@ -9,6 +13,10 @@ import android.util.Log;
 
 public class ThumbnailDownloader<T> extends HandlerThread {
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
+
+    private Handler mRequestHandler;
+    private ConcurrentMap<T,String> mRequestMap = new ConcurrentHashMap();
 
     public ThumbnailDownloader() {
         super(TAG);
@@ -16,5 +24,12 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     public void queueThumbnail (T target, String url){
         Log.i(TAG, "Got URL " + url);
+        if (url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
+            .sendToTarget();
+        }
     }
 }
