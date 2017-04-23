59c4e45fed2d100bac44a6766188a63f038690a8
Dawit Abraham <davescofab@gmail.com>
2017-04-23 20:52:36 +0300

    Log.i OnCreate in SingleFragmentActivity

--- app/src/main/java/com/qenetech/photogallery/SingleFragmentActivity.java
+++ app/src/main/java/com/qenetech/photogallery/SingleFragmentActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by davescof on 4/4/17.
@@ -14,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     public abstract Fragment createFragment ();
+    private static final String TAG = "SingleFragmentActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +30,8 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
             fm.beginTransaction().add(R.id.fragment_container, fragment)
                     .commit();
         }
+
+        Log.i(TAG, "================= On Create ====================");
     }
 
     @LayoutRes
